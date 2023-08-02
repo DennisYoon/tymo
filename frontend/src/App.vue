3511b31b52f92e9237c4daa055291bce6665755a
DennisYun <2008dennisyun@gmail.com>
2023-08-02 14:56:22 +0900

    [frontend] SelectionItem 대충 모양 구현

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,26 @@
 <template>
   <WindowLimit>
     <TopBar></TopBar>
+    <div id="ddddisssssco">
+      <SelectItem></SelectItem>
+    </div>
     <TypeArea></TypeArea>
   </WindowLimit>
+
 </template>
 
 <script lang="ts" setup>
   import WindowLimit from './components/WindowLimit.vue';
   import TopBar from './components/TopBar.vue';
   import TypeArea from "./components/TypeArea.vue";
+  import SelectItem from "./components/SelectItem.vue";
 </script>
 
 <style lang="scss">
   div {
     border: 0.5px dashed white;
+  }
+  #ddddisssssco {
+    padding: 100px;
   }
 </style>
